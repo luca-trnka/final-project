a58f971e6969cf9a5007f41e4397f1f3f7afde09
Martina Kročková <129508651+kroka6@users.noreply.github.com>
2023-08-08 09:05:05 +0200

    Added and updated not implemented page (#23)

--- frontend/src/Pages/Error.js
+++ frontend/src/Pages/Error.js
@@ -1,10 +1,47 @@
-function Error() {
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import {
+    MDBBtn,
+    MDBCard,
+    MDBCardBody,
+    MDBCardImage,
+    MDBCheckbox,
+    MDBCol,
+    MDBContainer,
+    MDBIcon,
+    MDBInput,
+    MDBRow
+} from 'mdb-react-ui-kit';
+
+const Error = () => {
+    const navigate = useNavigate();
+
+    const goHome = () => {
+        navigate('/');
+    };
+
     return (
-        <div className="page">
-            <h2>Error page</h2>
-        </div>
+        <MDBContainer fluid>
+            <MDBCard className='text-black m-5' style={{borderRadius: '25px'}}>
+                <MDBCardBody>
+                    <MDBRow>
+                        <MDBCol md='10' lg='6' className='order-2 order-lg-1 d-flex flex-column align-items-center'>
+                            <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Not implemented yet</p>
+                                <p>Sorry, this page is not available yet.</p>
+                            <MDBBtn className='mb-4' size='lg' onClick={goHome}>Go home</MDBBtn>
+                        </MDBCol>
+                        <MDBCol md='10' lg='6' className='order-1 order-lg-2 d-flex align-items-center'>
+                            <MDBCardImage
+                                src='https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp'
+                                fluid
+                            />
+
+                    </MDBCol>
+                </MDBRow>
+            </MDBCardBody>
+        </MDBCard>
+</MDBContainer>
     );
-    
-}
+};
 
 export default Error;
